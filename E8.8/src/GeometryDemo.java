9f62c15e37aff68eefe5739224ffba6144a4818f
nanami-tomoe <npnp123npnp123@gmail.com>
2023-11-09 15:34:53 +0900

    10주차

---
+++ E8.8/src/GeometryDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GeometryDemo
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter radius: ");
+        double radius = in.nextDouble();
+        System.out.println("Enter height: ");
+        double height = in.nextDouble();
+        // Add your code to solve the problem here
+
+    }
+}
