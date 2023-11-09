2b5be2c6341b9e63ff4a8978d68b0c876a653656
nanami-tomoe <npnp123npnp123@gmail.com>
2023-11-09 15:33:50 +0900

    10주차

--- E8.7/src/GeometryDemo.java
+++ E8.7/src/GeometryDemo.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
-public class GeometryDemo {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter radius:");
-        double r = in.nextDouble();
+public class GeometryDemo
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter radius: ");
+      double radius = in.nextDouble();
+      System.out.println("Enter height: ");
+      double height = in.nextDouble();
 
-        System.out.println("Enter height:");
-        double h = in.nextDouble();
-
-        Geometry geometry = new Geometry();
-
-        System.out.printf("Sphere volume: %.2f\n", geometry.sphereVolume(r));
-        System.out.printf("Sphere surface: %.2f\n", geometry.sphereSurface(r));
-        System.out.printf("Cylinder volume: %.2f\n", geometry.cylinderVolume(r, h));
-        System.out.printf("Cylinder volume: %.2f\n", geometry.cylinderSurface(r, h));
-        System.out.printf("Cone volume: %.2f\n", geometry.coneVolume(r, h));
-        System.out.printf("Cone volume: %.2f\n", geometry.coneSurface(r, h));
-    }
+      System.out.println("Sphere volume: " + Geometry.sphereVolume(radius));
+      System.out.println("Sphere surface: " + Geometry.sphereSurface(radius));
+      System.out.println("Cylinder volume: "
+            + Geometry.cylinderVolume(radius, height));
+      System.out.println("Cylinder surface: "
+            + Geometry.cylinderSurface(radius, height));
+      System.out.println("Cone volume: " + Geometry.coneVolume(radius, height));
+      System.out.println("Cone surface: "
+            + Geometry.coneSurface(radius, height));
+   }
 }
